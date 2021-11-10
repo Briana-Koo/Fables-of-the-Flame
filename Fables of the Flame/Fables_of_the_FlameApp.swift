202b0c8a2fd38c9fd9208e489db56acb616cc1b4
Briana-Koo <RoyalBlueSnow@gmail.com>
2021-11-10 10:28:06 -0600

    Initial Commit

---
+++ Fables of the Flame/Fables_of_the_FlameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fables_of_the_FlameApp.swift
+//  Fables of the Flame
+//
+//  Created by Student on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Fables_of_the_FlameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
